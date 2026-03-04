c15458fa632b3857af61f5a6628c6fbf21a5e3f0
MohamedAslam2001 <114648860+MohamedAslam2001@users.noreply.github.com>
2026-03-04 13:55:42 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".typing", {
+
+strings:[
+"Building scalable backend systems",
+"Java Microservices Developer",
+"Spring Boot & Angular Engineer",
+"Distributed Systems Enthusiast"
+],
+
+typeSpeed:60,
+backSpeed:40,
+loop:true
+
+});
